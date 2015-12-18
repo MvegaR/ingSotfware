0ff227ed9a3e2ca6399f6fb87de401e86f2e958c
Raul Opazo <raopazo@alumnos.ubiobio.cl>
2015-12-18 06:02:42 -0800

    Ultimas Validaciones y Error de Mayus
    Se agregaron las ultimas validaciones restantes: fechas, valores enteros
    mayor que cero, etc... Tambien se arreglo un error de escritura de una
    variable.

--- proyecto/models/TipoViajeForm.php
+++ proyecto/models/TipoViajeForm.php
@@ -2,10 +2,10 @@
 
 namespace app\models;
 use Yii;
-use yii\base\model;
+use yii\base\Model;
 use app\models\TipoViaje;
 
-class TipoViajeForm extends model{
+class TipoViajeForm extends Model{
 
     public $id_tipo_viaje;
     public $nombre_tipo_viaje;
@@ -15,9 +15,10 @@ public function rules()
  {
   return [
    ['nombre_tipo_viaje', 'required', 'message' => 'Campo requerido'],
-   ['nombre_tipo_viaje', 'match', 'pattern' => '/^[a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
-   ['monto_maximo', 'integer', 'message' => 'Monto incorrecto'],
+   ['nombre_tipo_viaje', 'string'],
+   ['monto_maximo', 'integer', 'message' => 'El monto debe ser un numero'],
    ['monto_maximo', 'required'],
+   ['monto_maximo', 'compare', 'compareValue' => 1, 'operator' => '>=', 'message' => 'Ingrese un valor mayor que 0'],
    ['id_tipo_viaje', 'integer'],
   ];
  }
